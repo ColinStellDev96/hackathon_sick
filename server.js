2f4b4d5ce29f0a5961bf3bf7c5e686d08d1ab3db
Kyle Estle <estlekyle@gmail.com>
2017-08-21 14:42:39 -0600

    I did things!

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+, router = express.Router()
+  , multer = require('multer')
+//Image uploading
+/////////////////
+var uploading = multer({
+  dest: __dirname + '../public/upload/',
+  limits: {fileSize: 1000000, files:1},
+})
+
+router.post('/upload', uploading, function(req, res) {
+
+})
+
+module.exports = router
+/////////////////
 var request = require('request');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
+
 
 var app = express();
 
@@ -14,6 +30,9 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.sendFile('/html/login.html', {root:'./public'});
+});
+app.get('/', function(req, res){
+    res.sendFile('/html/dashboard.html', {root:'./public'});
 });
 
 app.listen(8080);
