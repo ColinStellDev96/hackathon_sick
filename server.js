a0f1b02bcdb4a0df328de9bf73628d7e9ad73a2a
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-22 11:55:54 -0600

    rework login page

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ console.log("Server Started on Route 8080!");
 var app = express();
 var upload = require('express-fileupload');
 
+
+var MongoClient = mongo.MongoClient;
+var ObjectID = mongo.ObjectID;
+
 //
 app.use(express.static('./public'));
 
@@ -13,29 +17,51 @@ app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/login', function(req, res){
-    res.sendFile('/html/login.html', {root:'./public'});
-});
+MongoClient.connect('mongodb://localhost:27017/sick_fit', function(err, db) {
 
-app.get('/dashboard', function(req, res){
-    res.sendFile('/html/index.html', {root:'./public'});
-});
+    app.get('/login', function(req, res){
+        res.sendFile('/html/login.html', {root:'./public'});
+    });
 
-app.use(upload())
-app.get('/',function(req, res){
-  res.sendFile(__dirname+'/html/index.html', {root:'./public'})
-})
+    app.get('/dashboard', function(req, res){
+        res.sendFile('/html/index.html', {root:'./public'});
+    });
 
-app.post('/', function(req, res){
-  if (req.files){
-    var file = req.files.filename
-    console.log(req.files)
-  }
-})
+    app.use(upload())
+    app.get('/',function(req, res){
+      res.sendFile(__dirname+'/html/index.html', {root:'./public'})
+    })
 
-//css
-app.get('/main.css', function(req, res) {
-  res.sendFile('./main.css', {root: './styles'})
-})
+    app.post('/', function(req, res){
+      if (req.files){
+        var file = req.files.filename
+        console.log(req.files)
+      }
+    })
 
-app.listen(8080);
+    //css
+    app.get('/main.css', function(req, res) {
+      res.sendFile('./main.css', {root: './styles'})
+    })
+
+    //Login Page Data To Database
+    // SIGN UP
+    app.post('/userData', function (req, res){
+        console.log(req.body);
+        req.body['user_calorieGoal'] = parseInt(req.body['user_calorieGoal']);
+        db.collection('userData').insert(req.body, function(err){
+            console.log('err?', err);
+            res.send({success: 'success!'});
+        });
+    });
+
+
+    app.use(function(req, res, next) {
+        res.status(404).send('not found');
+    });
+    app.use(function(req, res, next) {
+        res.status(500).send('oops');
+    });
+    app.listen(8080);
+
+}); // END OF DATABASE CONNECT
