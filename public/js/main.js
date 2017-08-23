4aebdf6613e51689a109ee538fd5a2bc3f31b2d5
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-23 15:22:42 -0600

    mvp done

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         window.location = '/dashboard';
     });
 
+    // Submit User Data To Server & Database
     $('#signupForm').on('submit', function (event){
         event.preventDefault();
         console.log($(this).serialize());
@@ -16,6 +17,7 @@ $(document).ready(function(){
         });
     });
 
+    //Appending User Data to Dashboard
     $.get('/userData', function (data){
         console.log(data);
         var appendData = data;
@@ -26,6 +28,94 @@ $(document).ready(function(){
             $('#gender').append(`${appendData[appendData.length - 1]['user_gender']}`);
             $('#cal-goal').append(`${appendData[appendData.length - 1]['user_calorieGoal']}`);
     });
+
+    //Calorie Counter Variable
+    var calorieCount = 0;
+
+    // Displaying All Food Items in Table Data Form
+    $.get('/foodItems', function (data) {
+        console.log(data);
+        var foodAppend = data;
+        for (var i = 0; i < foodAppend.length; i++) {
+            $('#foodData').append(`
+                <tr>
+                    <td>${foodAppend[i]['foodName']}</td>
+                    <td>${foodAppend[i]['calories']}</td>
+                    <td><input type="checkbox" class="foodCheck"></td>
+                </tr>`
+            );
+        }
+        // for (key in foodAppend) {
+        //     if (key === '_id') {
+        //         console.log(_id);
+        //     }
+        // } //end of for in loop
+        $('.foodCheck').click(function (){
+            // console.log($(this.checked))
+            $('#totalCount').empty()
+            var thisCals = $($(this).parent().parent()).children()[1].innerText;
+            if (this.checked == true) {
+                calorieCount += parseInt(thisCals);
+                $('#totalCount').text(calorieCount);
+            } else if (this.checked == false) {
+                calorieCount -= parseInt(thisCals);
+                $('#totalCount').text(calorieCount);
+            }
+        });
+    });
+
+    //Displaying All Workout Items in Table Data Form
+    $.get('/calories', function (data) {
+        console.log(data);
+        var exerciseAppend = data;
+        for (var i = 0; i < exerciseAppend.length; i++) {
+            $('#workoutData').append(`
+                <tr>
+                    <td>${exerciseAppend[i]['excerciseName']}</td>
+                    <td>${exerciseAppend[i]['calories']}</td>
+                    <td><input type="checkbox" class="workCheck"></td>
+                </tr>`
+            );
+        }
+
+        $('.workCheck').click(function (){
+            // console.log($(this.checked))
+            $('#totalCount').empty()
+            var thisCals = $($(this).parent().parent()).children()[1].innerText;
+            if (this.checked == true) {
+                calorieCount -= parseInt(thisCals);
+                $('#totalCount').text(calorieCount);
+            } else if (this.checked == false) {
+                calorieCount += parseInt(thisCals);
+                $('#totalCount').text(calorieCount);
+            }
+        });
+
+    });
+
+    //Adding Calories to Calorie Count
+    // $('#foodCheck').click(function (){
+    //     for (var key in foodAppend) {
+    //         if (key === _id) {
+    //             console.log(_id);
+    //         }
+    //     }
+    // });
+
+
+    //Removing Calories From Calorie Count
+
+
+
+
+
+
+
+
+
+        // $.get('/api_data', function (data){
+        //     console.log(data);
+        // });
 
 
 
--- server.js
+++ server.js
@@ -33,18 +33,6 @@ MongoClient.connect('mongodb://localhost:27017/sick_fit', function(err, db) {
       res.sendFile('./main.css', {root: './styles'});
   });
 
-    app.use(upload());
-    app.get('/',function(req, res){
-      res.sendFile(__dirname+'/html/index.html', {root:'./public'});
-  });
-
-    app.post('/', function(req, res){
-      if (req.files){
-        var file = req.files.filename;
-        console.log(req.files);
-      }
-  });
-
     //Login Page Data To Database
     // SIGN UP
     app.post('/userData', function (req, res){
@@ -56,6 +44,7 @@ MongoClient.connect('mongodb://localhost:27017/sick_fit', function(err, db) {
         });
     });
 
+    // Grabbing User Data From Database and Sending to Front-End
     app.get('/userData', function (req, res){
         db.collection('userData').find({}).toArray(function(err, docs) {
             console.log('err?', err);
@@ -63,20 +52,54 @@ MongoClient.connect('mongodb://localhost:27017/sick_fit', function(err, db) {
         });
     });
 
+    // Grabbing Food Data From Database and Sending to Front-End
+    app.get('/foodItems', function (req, res){
+        db.collection('foodItems').find({}).sort({'foodName': 1}).toArray(function(err, docs){
+            console.log('err?', err);
+            res.send(docs);
+        });
+    });
+
+    // Grabbing Workout Data From Database and Sending to Front-End
+    app.get('/calories', function (req, res){
+        db.collection('exercise').find({}).sort({'excerciseName': 1}).toArray(function(err, docs){
+            console.log('err?', err);
+            res.send(docs);
+        });
+    });
+
+
+
+
+
+
+
+    // app.get('/api_data', function (req, res) {
+    //     console.log('working');
+    //     request('https://trackapi.nutritionix.com/v2/search/instant&appId=72192468&appKey=46bc41cc62b1c33c0c1adeb162e3a8bc', function (error, response, body){
+    //         console.log(body);
+    //         console.log('error?', error);
+    //         var apiData = JSON.parse(body);
+    //         res.send(apiData);
+    //     });
+    // });
+
+
+
 //File Uploading
 app.use(upload());
 app.get('/',function(req, res){
   res.sendFile(__dirname+'/html/index.html', {root:'./public'});
 });
-// >>>>>>> 73390fd576ab0efa7ed8f83a8a1752cfed380253
 
-
+// END OF APP Error Messages
     app.use(function(req, res, next) {
         res.status(404).send('not found');
     });
     app.use(function(req, res, next) {
         res.status(500).send('oops');
     });
+// APP LISTEN
     app.listen(8080);
 
 }); // END OF DATABASE CONNECT
